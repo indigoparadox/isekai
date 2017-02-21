8506fedb7f7439729cc65a18352030eb41a2d15f
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2017-02-21 12:11:38 -0500

    Switched to the plain vanilla XML map format.

--- src/tilemap.c
+++ src/tilemap.c
@@ -1,7 +1,7 @@
 
 #include "tilemap.h"
 
-#include "json/json.h"
+#include "ezxml/ezxml.h"
 
 #include <memory.h>
 #include <string.h>
@@ -41,179 +41,106 @@ void tilemap_position_cleanup( TILEMAP_POSITION* position ) {
 
 }
 
-typedef struct {
-    struct tagbstring path;
-    json_type type;
-    /* Return TRUE to continue descending, or FALSE to not. */
-    BOOL (*callback)( TILEMAP* t, json_value* j, bstring path );
-} tilemap_loader_entry;
+static void tilemap_parse_properties( TILEMAP* t, ezxml_t xml_props ) {
+    ezxml_t xml_prop_iter = NULL;
 
-static BOOL tilemap_parse_layer( TILEMAP* t, json_value* j, bstring path ) {
-    TILEMAP_LAYER* layer = NULL;
-    int layer_index;
-    struct bstrList* path_list = NULL;
-    char* base_cstr = NULL;
+    scaffold_check_null( xml_props );
 
-    /* Figure out the layer index. */
-    path_list = bsplit( path, '/' );
-    scaffold_check_null( path_list );
-    base_cstr = bdata( path_list->entry[path_list->qty - 1] );
-    scaffold_check_null( base_cstr );
-    layer_index = atoi( base_cstr );
+    xml_prop_iter = ezxml_child( xml_props, "property" );
 
-    /* Create the layer if it's missing. */
-    layer = vector_get( &(t->layers), layer_index );
-    if( NULL == layer ) {
-        /* We're starting a new layer. */
-        /* TODO: Allow for out-of-order layers? */
-        tilemap_layer_new( layer );
+    while( NULL != xml_prop_iter ) {
+        //if( 0 == strcmp( ezxml_attr( xml_prop_iter, "name" ), "light_str" ) ) {
+        //    map_out->light_str = atoi( ezxml_attr( xml_prop_iter, "value" ) );
 
-        vector_add( &(t->layers), layer );
+        //if( 0 == strcmp( ezxml_attr( xml_prop_iter, "name" ), "music_path" ) ) {
+        //    map_out->music_path = bfromcstr( ezxml_attr( xml_prop_iter, "value" ) );
 
-        layer->tiles = calloc( j->u.array.length, sizeof( uint16_t ) );
-        scaffold_check_null( layer->tiles );
+        //if( 0 == strcmp( ezxml_attr( xml_prop_iter, "name" ), "time_moves" ) ) {
+        //    if( 0 == strcmp( ezxml_attr( xml_prop_iter, "value" ), "true" ) ) {
+        //       map_out->time_moves = TRUE;
+        //    } else {
+        //       map_out->time_moves = FALSE;
+        //    }
+
+        xml_prop_iter = xml_prop_iter->next;
     }
 
 cleanup:
-    bstrListDestroy( path_list );
-    return TRUE;
+    return;
 }
 
-static BOOL tilemap_parse_tile( TILEMAP* t, json_value* j, bstring path ) {
+void tilemap_parse_tiledata( TILEMAP* t, ezxml_t xml_tiledata ) {
+    scaffold_check_null( xml_tiledata );
+
+cleanup:
+    return;
+}
+
+void tilemap_parse_layer( TILEMAP* t, ezxml_t xml_layer ) {
     TILEMAP_LAYER* layer = NULL;
-
-    /* This must be a tile. */
-    layer = vector_get( &(t->layers), (vector_count( &(t->layers) ) - 1) );
-    scaffold_check_null( layer );
-    scaffold_check_null( layer->tiles );
-    scaffold_check_bounds( layer->tiles_count, layer->tiles_alloc );
-
-    layer->tiles[layer->tiles_count++] = j->u.integer;
-
-cleanup:
-    return TRUE;
-}
-
-static BOOL tilemap_parse_tileset_properties( TILEMAP* t, json_value* j, bstring path ) {
-    return FALSE;
-}
-
-static BOOL tilemap_parse_tileset( TILEMAP* t, json_value* j, bstring path ) {
-    struct bstrList* path_list = NULL;
-
-    path_list = bsplit( path, '/' );
-
-    if( 0 == bstrcmp( scaffold_static_string( "properties" ), path_list->entry[path_list->qty - 1] ) ) {
-        tilemap_parse_tileset_properties( t, j, path );
-        goto cleanup;
-    }
-
-
-
-cleanup:
-    bstrListDestroy( path_list );
-    return FALSE;
-}
-
-static const tilemap_loader_entry tilemap_loader_table[] = {
-    {bsStatic( "/layers" ), json_array, tilemap_parse_layer},
-    {bsStatic( "/layers/data" ), json_integer, tilemap_parse_tile},
-    {bsStatic( "/tilesets" ), json_object, tilemap_parse_tileset},
-    {bsStatic( "" ), json_none, NULL}
-};
-
-static void tilemap_parse_walk(
-    TILEMAP* t, json_value* j, bstring path, int depth
-) {
+    ezxml_t xml_layer_data = NULL;
     bstring buffer = NULL;
+    struct bstrList* tiles_list = NULL;
     int i;
-    const tilemap_loader_entry* loader_entry = tilemap_loader_table;
-    BOOL destroy_path = FALSE;
 
-    if( NULL == path ) {
-        path = bfromcstralloc( 30, "" );
-        destroy_path = TRUE;
-        scaffold_check_null( path );
+    scaffold_check_null( xml_layer );
+
+    tilemap_layer_new( layer );
+
+    layer->width = atoi( ezxml_attr( xml_layer, "width" ) );
+    layer->height = atoi( ezxml_attr( xml_layer, "height" ) );
+
+    xml_layer_data = ezxml_child( xml_layer, "data" );
+    scaffold_check_null( xml_layer_data );
+
+    buffer = bfromcstr( ezxml_txt( xml_layer_data ) );
+    scaffold_check_null( buffer );
+    tiles_list = bsplit( buffer, ',' );
+    scaffold_check_null( tiles_list );
+    layer->tiles_alloc = tiles_list->qty;
+    layer->tiles = calloc( layer->tiles_alloc, sizeof( uint16_t ) );
+    scaffold_check_null( layer->tiles );
+
+    for( i = 0 ; tiles_list->qty > i ; i++ ) {
+        layer->tiles[i] = atoi( (bdata( tiles_list->entry[i] )) );
+        layer->tiles_count++;
     }
 
-#ifdef DEBUG_JSON
-    for( i = 0 ; depth > i ; i++ ) {
-        fprintf( stderr, "." );
-    }
-
-    if( NULL != name ) {
-        fprintf( stderr, "%s: ", bdata( name ) );
-    }
-#endif /* DEBUG_JSON */
-
-    while( NULL != loader_entry->callback ) {
-        if(
-            0 == bstrncmp(
-                &(loader_entry->path), path, blength( &(loader_entry->path) )
-            ) && json_array == j->type
-        ) {
-            if( loader_entry->callback( t, j, path ) ) {
-                goto cleanup;
-            }
-        }
-        loader_entry++;
-    }
-
-    switch( j->type ) {
-    case json_object:
-        for( i = 0 ; j->u.object.length > i ; i++ ) {
-            buffer = bstrcpy( path );
-            scaffold_check_null( buffer );
-            bconchar( buffer, '/' );
-            binsertblk( buffer, blength( buffer ), j->u.object.values[i].name, j->u.object.values[i].name_length, '\0' );
-            scaffold_print_debug( "Descending into path: %s\n", bdata( buffer ) );
-            tilemap_parse_walk( t, j->u.object.values[i].value, buffer, depth + 1 );
-            bdestroy( buffer );
-        }
-        break;
-
-    case json_array:
-        for( i = 0 ; j->u.array.length > i ; i++ ) {
-            buffer = bformat( "%s/%d", bdata( path ), i );
-            scaffold_check_null( buffer );
-            scaffold_print_debug( "Descending into path: %s\n", bdata( buffer ) );
-            tilemap_parse_walk( t, j->u.array.values[i], buffer, depth + 1 );
-            bdestroy( buffer );
-        }
-        break;
-
-#ifdef DEBUG_JSON
-    case json_integer:
-        fprintf( stderr, "%d\n", j->u.integer );
-        break;
-
-    case json_string:
-        fprintf( stderr,"%s\n", j->u.string.ptr );
-        break;
-
-    default:
-        fprintf( stderr,"\n" );
-        break;
-#endif /* DEBUG_JSON */
-    }
+    vector_add( &(t->layers), layer );
 
 cleanup:
-    if( TRUE == destroy_path ) {
-        bdestroy( path );
+    if( SCAFFOLD_ERROR_NONE != scaffold_error ) {
+        tilemap_layer_free( layer );
     }
+    bdestroy( buffer );
+    bstrListDestroy( tiles_list );
     return;
 }
 
 void tilemap_load_data( TILEMAP* t, const uint8_t* tmdata, int datasize ) {
-    json_value* tmapjson = NULL;
+    ezxml_t xml_map = NULL, xml_layer = NULL, xml_props = NULL,
+        xml_tiledata = NULL;
 
-    tmapjson = json_parse( (json_char*)tmdata, datasize );
-    scaffold_check_null( tmapjson );
+    xml_map = ezxml_parse_str( tmdata, datasize );
 
-    tilemap_parse_walk( t, tmapjson, NULL, 0 );
+    xml_tiledata = ezxml_child( xml_map, "tileset" );
+    tilemap_parse_tiledata( t, xml_tiledata );
+
+    xml_props = ezxml_child( xml_map, "properties" );
+    tilemap_parse_properties( t, xml_props );
+
+    /* Load the map's tile data. */
+    //map_out->tileset = tilemap_create_tileset( tiledata_path );
+
+    xml_layer = ezxml_child( xml_map, "layer" );
+    while( NULL != xml_layer ) {
+        tilemap_parse_layer( t, xml_layer );
+        xml_layer = xml_layer->next;
+    }
+
 
 cleanup:
-    json_value_free( tmapjson );
+    ezxml_free( xml_map );
     return;
 }
 
