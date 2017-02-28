43ed0b766f8e7ed971c94c8b588f87d5fd0683e6
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2017-02-28 10:44:43 -0500

    Hashmap check is basically functional.

---
+++ test/check_data.c
@@ -0,0 +1,2 @@
+
+#include "check_data.h"BLOB* create_blob( uint32_t sent_s, uint16_t ptrn, size_t c, uint32_t sent_e ) {   size_t i;   BLOB* blob;   blob = (BLOB*)calloc( 1, sizeof( BLOB ) );   blob->sentinal_start = sent_s;   blob->data_len = c;   blob->data = (uint16_t*)calloc( c, sizeof( uint16_t ) );   for( i = 0 ; c > i ; i++ ) {      blob->data[i] = ptrn;   }   blob->sentinal_end = sent_e;   return blob;}void free_blob( BLOB* blob ) {   free( blob->data );   free( blob );}
