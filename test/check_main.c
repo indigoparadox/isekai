a672f63397ba7f57d6a38b04fe5d34a3634e262f
indigoparadox <indigo.repo@interfinitydynamics.info>
2017-03-07 00:35:13 -0500

    Added check_syncbuff test.

--- test/check_main.c
+++ test/check_main.c
@@ -9,7 +9,8 @@ main_add_test_proto( b64 );
 main_add_test_proto( channel );
 main_add_test_proto( hashmap );
 main_add_test_proto( chunker );
-main_add_test_proto( connection );
+//main_add_test_proto( connection );
+main_add_test_proto( syncbuff );
 
 #define main_add_test( suite_name ) \
    Suite* s_ ## suite_name = suite_name ## _suite(); \
@@ -23,11 +24,12 @@ int main( void ) {
 
    main_add_test( vector );
    main_add_test( b64 );
-   main_add_test( connection );
+   //main_add_test( connection );
    //main_add_test( channel );
    //main_add_test( client );
    main_add_test( hashmap );
    main_add_test( chunker );
+   main_add_test( syncbuff );
 
    return( number_failed == 0 ) ? 0 : 1;
 }
