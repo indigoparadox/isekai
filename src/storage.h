0ff929c1f9ed5652d02bf76fb616125f87aa9f08
indigoparadox <1065446+indigoparadox@users.noreply.github.com>
2019-01-01 02:48:54 -0500

    Still murky on storage, started implementing using vectors, but also decided on a key-value store which can't store arrays.

--- src/storage.h
+++ src/storage.h
@@ -4,13 +4,23 @@
 
 #include <libvcol.h>
 
+enum STORAGE_VALUE_TYPE {
+   STORAGE_VALUE_NONE,
+   STORAGE_VALUE_INT,
+   STORAGE_VALUE_STRING
+};
+
+enum STORAGE_HALF {
+   STORAGE_HALF_CLIENT,
+   STORAGE_HALF_SERVER
+};
+
 #define STORAGE_VERSION_CURRENT 1
 
 VBOOL storage_init();
 void storage_free();
-bstring storage_client_get_string( bstring key );
-void storage_client_set_string( bstring key, bstring str );
-int storage_client_get_int( bstring key );
-void storage_client_set_int( bstring key, int val );
+bstring storage_client_get_string( bstring store, bstring key );
+void storage_client_set_string( bstring store, bstring key, bstring str );
+int storage_get( bstring store, enum STORAGE_HALF half, bstring key, enum STORAGE_VALUE_TYPE type, struct VECTOR* out );
 
 #endif /* STORAGE_H */
--- src/storage/sqlite.c
+++ src/storage/sqlite.c
@@ -3,19 +3,18 @@
 
 #include <sqlite3.h>
 
-enum STORAGE_HALF {
-   STORAGE_HALF_CLIENT,
-   STORAGE_HALF_SERVER
-};
-
 static sqlite3* db = NULL;
 
-static struct tagbstring str_version = bsStatic( "version" );
-static struct tagbstring qry_stmt = bsStatic( "SELECT value FROM ? WHERE half=? AND key=?" );
+struct tagbstring str_version = bsStatic( "version" );
+struct tagbstring str_settings = bsStatic( "settings" );
+struct tagbstring str_sql_select = bsStatic( "SELECT value FROM %s WHERE half = ? AND key = ?" );
+struct tagbstring qry_insert = bsStatic( "INSERT INTO %s (half, key, value) VALUES (?, ?, ?)" );
+struct tagbstring qry_update = bsStatic( "UPDATE %s SET value=? WHERE half = ? AND key = ?" );
 
 VBOOL storage_init() {
-   int res = VFALSE,
-      version = 0;
+   int res = VFALSE;
+   struct VECTOR* version = NULL;
+   int* version_scalar = NULL;
 
    res = sqlite3_open( "storage.db", &db );
 
@@ -32,14 +31,27 @@ VBOOL storage_init() {
       NULL
    );
 
-   version = storage_client_get_int( &str_version );
-   if( STORAGE_VERSION_CURRENT < version ) {
-      lg_error( __FILE__, "Client storage version is too new: %d\n", version );
+   version = vector_new();
+   if( 0 == storage_get( &str_settings, STORAGE_HALF_SERVER, &str_version, STORAGE_VALUE_INT, version ) ) {
+      lg_error( __FILE__, "Version key missing. Data corrupt!\n" );
+      storage_client_set_int( &str_settings, &str_version, STORAGE_VERSION_CURRENT );
       res = VTRUE;
       goto cleanup;
    }
+   version_scalar = vector_get( version, 0 );
+   if( STORAGE_VERSION_CURRENT < *version_scalar ) {
+      lg_error( __FILE__, "Client storage version is too new: %d\n", version );
+      res = VTRUE;
+      goto cleanup;
+   } else if( STORAGE_VERSION_CURRENT > *version_scalar ) {
+      lg_error( __FILE__, "Client storage version is too old: %d\n", version );
+      /* TODO: Upgrade. */
+      //res = VTRUE;
+      //goto cleanup;
 
-   storage_client_set_int( &str_version, STORAGE_VERSION_CURRENT );
+      /* Save the new version to database after upgrading (if applicable). */
+      storage_client_set_int( &str_settings, &str_version, STORAGE_VERSION_CURRENT );
+   }
 
 cleanup:
    return res;
@@ -50,69 +62,100 @@ void storage_free() {
    db = NULL;
 }
 
-static sqlite3_stmt* _storage_create_select(
-   enum STORAGE_HALF half, bstring key
+#if 0
+static sqlite3_stmt* _storage_create_insert_update(
+   bstring store, enum STORAGE_HALF half, bstring key
 ) {
-   sqlite3_stmt* out = NULL;
+   sqlite3_stmt* out = NULL,
+      * test_present = NULL;
    int res = 0;
 
-   sqlite3_prepare( db, bdata( &qry_stmt ), blength( &qry_stmt ), &out, NULL );
+   _storage_create_select( store, half, key );
+
+   if( SQLITE_ROW == sqlite3_step( test_present ) ) {
+      lg_info( __FILE__, "Value exists, updating: %b\n", key );
+   } else {
+      lg_info( __FILE__, "Storing new value for: %b\n", key );
+   }
+
+   sqlite3_prepare( db, bdata( &str_sql_select ), blength( &str_sql_select ), &out, NULL );
    lgc_null( out );
 
    res = sqlite3_bind_text( out, 1, bdata( key ), blength( key ), SQLITE_STATIC );
    lgc_equal( res, SQLITE_OK );
 
 cleanup:
+   if( NULL == test_present ) {
+      lg_error( __FILE__, "%s\n", sqlite3_errstr( sqlite3_errcode( db ) ) );
+   } else {
+      /* We won't need this again. */
+      sqlite3_finalize( test_present );
+   }
    return out;
 }
+#endif // 0
 
-static sqlite3_stmt* _storage_create_insert(
-   enum STORAGE_HALF half, bstring key
+int storage_get(
+   bstring store,
+   enum STORAGE_HALF half,
+   bstring key,
+   enum STORAGE_VALUE_TYPE type,
+   void* out
 ) {
-   sqlite3_stmt* out = NULL;
-   int res = 0;
-
-   sqlite3_prepare( db, bdata( &qry_stmt ), blength( &qry_stmt ), &out, NULL );
-   lgc_null( out );
-
-   res = sqlite3_bind_text( out, 1, bdata( key ), blength( key ), SQLITE_STATIC );
-   lgc_equal( res, SQLITE_OK );
-
-cleanup:
-   return out;
-}
-
-bstring storage_client_get_string( bstring key ) {
-
-}
-
-void storage_client_set_string( bstring key, bstring str ) {
-}
-
-int storage_client_get_int( bstring key ) {
    sqlite3_stmt* stmt = NULL;
    int res = 0,
-      out = 0;
+      ret_count = 0,
+      * ret_int = 0;
+   bstring str_sql_temp = NULL;
 
-   stmt = _storage_create_select( STORAGE_HALF_CLIENT, key );
+   str_sql_temp = bformat( bdata( &str_sql_select ), bdata( store ) );
+   lgc_null( str_sql_temp );
+
+   lg_debug( __FILE__, "SQL: %b\n", str_sql_temp );
+   sqlite3_prepare_v2( db, bdata( str_sql_temp ), -1, &stmt, NULL );
    lgc_null( stmt );
 
-   if( SQLITE_ROW == sqlite3_step( stmt ) ) {
-      out = sqlite3_column_int( stmt, 0 );
-      lg_info( __FILE__, "Storage retrieved: %b: %d\n", key, out );
+   res = sqlite3_bind_int( stmt, 1, (int)half );
+   lgc_equal( res, SQLITE_OK );
+
+   res = sqlite3_bind_text( stmt, 2, bdata( key ), blength( key ), SQLITE_STATIC );
+   lgc_equal( res, SQLITE_OK );
+
+   while( SQLITE_ROW == sqlite3_step( stmt ) ) {
+      switch( type ) {
+      case STORAGE_VALUE_INT:
+         ret_int = mem_alloc( 1, int );
+         lgc_null( ret_int );
+         *out = sqlite3_column_int( stmt, 0 );
+         out = ret_int;
+         ret_int = NULL;
+         ret_count++;
+         lg_debug( __FILE__, "Storage retrieved: %b: %d\n", key, *ret_int );
+         break;
+      }
    }
 
 cleanup:
-   sqlite3_finalize( stmt );
-   return out;
+   if( NULL == stmt ) {
+      lg_error( __FILE__, "%s\n", sqlite3_errstr( sqlite3_errcode( db ) ) );
+   } else {
+      sqlite3_finalize( stmt );
+   }
+   return ret_count;
 }
 
-void storage_client_set_int( bstring key, int val ) {
+void storage_set(
+   bstring store,
+   enum STORAGE_HALF half,
+   bstring key,
+   enum STORAGE_VALUE_TYPE type,
+   void* value
+) {
    sqlite3_stmt* stmt = NULL;
    int res = 0,
       test = 0;
 
-   stmt = _storage_create_insert( STORAGE_HALF_CLIENT, key );
+   //stmt = _storage_create_insert_update( store, STORAGE_HALF_CLIENT, key );
    lgc_null( stmt );
 
    res = sqlite3_bind_int( stmt, 2, val );
